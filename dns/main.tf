160db267ea2e88256a6c3ae670575d19aba92b2f
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-25 20:06:34 +0100

    Renamed to be more descriptive

--- dns/main.tf
+++ dns/main.tf
@@ -105,7 +105,7 @@ resource "digitalocean_record" "google_verify_forms" {
   value  = "google-site-verification=ryrYCeqvEF5EDnpjioRq1DIyY6PByK-LbtkFwcI7m-c"
 }
 
-resource "digitalocean_record" "login_a" {
+resource "digitalocean_record" "login_wildcard_a" {
   domain = var.login
   type   = "A"
   name   = "*"
