cb46e17207018591fdb9f2584ae5f7f98cbe4f2c
gjermundhp02 <gjermund.pedersen@gmail.com>
2026-04-28 19:46:15 +0200

    feat: move domains from k8s

---
+++ dns/domain_cnames.tf
@@ -0,0 +1,14 @@
+locals {
+  excluded = ["logout.no", var.login]
+  domain_names = [ for d in module.domeneshop.domain_names : d if contains(local.excluded, d) == false ]
+}
+
+resource "digitalocean_record" "cname_records" {
+  for_each = toset(local.domain_names)
+
+  domain = var.login
+  name   = each.value
+  type   = "CNAME"
+  value  = "${var.login}."
+  
+}
